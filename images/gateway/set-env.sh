1af793c37603bb1538d47329ae994ec37dab1e8f
Marcel Alberto Martinelli <marcel.martinelli@mpt.mp.br>
2018-05-18 14:03:17 -0300

    Change to connect in zookeeper to get confs

---
+++ images/gateway/set-env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Setup environment variables to redis and elastic
+GRAVITEEIO_REDISDB_HOST=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/redisdb_host" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
+GRAVITEEIO_REDISDB_PORT=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/redisdb_port" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
+GRAVITEEIO_ELASTIC_HOST=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/elastic_port" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
+GRAVITEEIO_ELASTIC_PORT=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/elastic_port" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
+
+export GRAVITEEIO_REDISDB_HOST
+export GRAVITEEIO_REDISDB_PORT
+export GRAVITEEIO_ELASTIC_HOST
+export GRAVITEEIO_ELASTIC_PORT
---
+++ images/management-api/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+. "$GRAVITEEIO_HOME"/bin/set-env.sh
+. "$GRAVITEEIO_HOME"/bin/gravitee
