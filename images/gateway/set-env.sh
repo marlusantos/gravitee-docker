ebc8355cb863c8a2bf0ab5d3cd44e77e8f5dab51
Marcel Alberto Martinelli <marcel.martinelli@mpt.mp.br>
2018-05-18 17:06:17 -0300

    Change script to set-env

--- images/gateway/set-env.sh
+++ images/gateway/set-env.sh
@@ -6,7 +6,10 @@ GRAVITEEIO_REDISDB_PORT=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravite
 GRAVITEEIO_ELASTIC_HOST=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/elastic_host" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
 GRAVITEEIO_ELASTIC_PORT=$(/opt/zookeeper-cli-1.4/bin/zk -c "get -s /spai/gravitee/elastic_port" "$ZOOKEEPER_HOST":"$ZOOKEEPER_PORT" | tail -n +2)
 
-export GRAVITEEIO_REDISDB_HOST
-export GRAVITEEIO_REDISDB_PORT
-export GRAVITEEIO_ELASTIC_HOST
-export GRAVITEEIO_ELASTIC_PORT
+echo "export GRAVITEEIO_REDISDB_HOST=$GRAVITEEIO_REDISDB_HOST" >> /etc/profile.d/gravitee-env.sh
+echo "export GRAVITEEIO_REDISDB_PORT=$GRAVITEEIO_REDISDB_PORT" >> /etc/profile.d/gravitee-env.sh
+echo "export GRAVITEEIO_ELASTIC_HOST=$GRAVITEEIO_ELASTIC_HOST" >> /etc/profile.d/gravitee-env.sh
+echo "export GRAVITEEIO_ELASTIC_PORT=$GRAVITEEIO_ELASTIC_PORT" >> /etc/profile.d/gravitee-env.sh
+
+chmod +x /etc/profile.d/gravitee-env.sh
+source /etc/profile
